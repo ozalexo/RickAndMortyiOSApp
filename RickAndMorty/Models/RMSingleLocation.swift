3cac3deb221d97c5c49c1ea7a2ec83f360febc0e
Alexey Ozerov <alexozerov@gmail.com>
2023-03-27 01:37:51 +0300

    Set Character, Location and Episode models

---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Alexey Ozerov on 27.03.2023.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable {
+    let name: String
+    let url: String
+}
